288abb9c2b40470900b0c8d92974c62fe4f32286
Bextraordinary <badrlord7070@gmail.com>
2026-05-05 19:08:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.getElementById('dice').onclick = function() {
+    let number = Math.floor(Math.random() * 6) + 1;
+    document.getElementById('result').innerText = 'طلع ليك: ' + number + ' 🎲';
+    
+    let dice = document.getElementById('dice');
+    dice.style.transform = 'scale(0.95)';
+    setTimeout(() => { dice.style.transform = 'scale(1)'; }, 100);
+};
